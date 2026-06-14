6432e121c8385a402bebcc5722fa380c90027fc8
isara <issarapichairat@gmail.com>
2026-06-14 14:38:38 +0700

    test controller

--- src/song/song.controller.spec.ts
+++ src/song/song.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SongController } from './song.controller';
+import { SongService } from './song.service';
+import exp from 'constants';
+import { CreateSongDTO } from './dto/create-song-dto';
+import { UpdateSongDTO } from './dto/update-song-dto';
+import { title } from 'process';
 
 describe('SongController', () => {
   let controller: SongController;
@@ -7,6 +12,32 @@ describe('SongController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SongController],
+      providers: [
+        {
+          provide: SongService,
+          useValue: {
+
+            getSongs: jest.fn().mockResolvedValue([{ id: 1, title: 'Dansing Feat' }]),
+            getSong: jest.fn().mockImplementation((id: string) => {
+              return Promise.resolve({ id: id, title: "Dancing" });
+            }),
+            createSong: jest
+              .fn()
+              .mockImplementation((createSongDTO: CreateSongDTO) => {
+                return Promise.resolve({ id: "a uuid", ...createSongDTO });
+              }),
+            updateSong: jest
+              .fn()
+              .mockImplementation((updateSongDTO: UpdateSongDTO) => {
+                return Promise.resolve({ affected: 1 });
+              }),
+            deleteSong: jest.fn().mockImplementation((id: string) => {
+              return Promise.resolve({ affected: 1 });
+            }),
+          },
+
+        },
+      ],
     }).compile();
 
     controller = module.get<SongController>(SongController);
@@ -15,4 +46,49 @@ describe('SongController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('getSongs', () => {
+
+    it('should fetch all the song', async () => {
+      const songs = await controller.getSongs();
+      expect(songs).toEqual([{ id: 1, title: 'Dansing Feat' }]);
+    });
+  });
+
+  describe("getSong by id", () => {
+    it("should give me the song by id", async () => {
+      const song = await controller.getSong("a uuid");
+      expect(song.id).toBe("a uuid");
+    });
+  });
+
+  describe("createSong", () => {
+    it("should create a new song", async () => {
+      const newSongDTO: CreateSongDTO = {
+        title: "Runaway",
+      }; const song = await controller.createSong(newSongDTO);
+      expect(song.title).toBe("Runaway");
+      expect(song).toEqual({id:'a uuid',title:'Runaway'})
+    });
+  });
+
+  describe("updateSong", () => {
+    it("should update the song DTO", async () => {
+      const updatesongDTO: UpdateSongDTO = {
+        title: "Animals",
+      };
+      const updateResults = await controller.updateSong("a uuid", updatesongDTO);
+      expect(updateResults).toBeDefined();
+      expect(updateResults.affected).toBe(1);
+    });
+  });
+
+  describe("deleteSong", () => {
+    it("should delete the song", async () => {
+      const deleteResult = await controller.deleteSong("a uuid");
+      expect(deleteResult.affected).toBe(1);
+    });
+  });
+
+
 });
